b75462b92641bbee9ca53ff28216f3b372a74b2b
weiche9835 <57296603+weiche9835@users.noreply.github.com>
2020-03-01 15:06:46 +0800

    Add files via upload

--- ImageEstimate.py
+++ ImageEstimate.py
@@ -37,35 +37,37 @@ def Calssim(gray,gray2):
     gray = np.float32(gray)
     gray2 = np.float32(gray2)
     ans = 0
-    for u in range(0,gray.shape[0],8):
-        for v in range(0,gray.shape[1],8):
-            C1 = (0.01*255)**2 #C1 = (K1*L)**2
-            C2 = (0.03*255)**2 #C2 = (K2*L)**2
-            C3 = C2/2 #C3 = C2/2
-            meanx = MEAN(gray,u,v)
-            meany = MEAN(gray2,u,v)
-            
-            msex = MSE(gray,meanx,u,v)
-            msey = MSE(gray2,meany,u,v)
-            
-            cc = CC(gray,gray2,meanx,meany,u,v)
-            
-            #l(x,y)
-            lxy = (2*meanx*meany + C1)/(meanx**2 + meany**2 + C1)
-            #c(x,y)
-            cxy = (2*msex*msey + C2)/(msex**2 + msey**2 + C2)
-            #s(x,y)
-            sxy = (cc + C3)/(msex*msey + C3)
-            
-            ssim = lxy*cxy*sxy
-            ''' the same funciton to calculate ssim
-            ssim = (2*meanx*meany + C1)*(2*cc + C2)
-            ssim = ssim/(((meanx**2) + (meany**2) + C1)*((msex**2) + (msey**2) + C2))
-            '''
-            ans = ans + ssim
-    ans = ans/((gray.shape[0]/8)*(gray.shape[1]/8))
-    return ans
-
+    if(gray.shape[0]==gray2.shape[0]) and (gray.shape[1]==gray2.shape[1]):
+        for u in range(0,gray.shape[0],8):
+            for v in range(0,gray.shape[1],8):
+                C1 = (0.01*255)**2 #C1 = (K1*L)**2
+                C2 = (0.03*255)**2 #C2 = (K2*L)**2
+                C3 = C2/2 #C3 = C2/2
+                meanx = MEAN(gray,u,v)
+                meany = MEAN(gray2,u,v)
+                
+                msex = MSE(gray,meanx,u,v)
+                msey = MSE(gray2,meany,u,v)
+                
+                cc = CC(gray,gray2,meanx,meany,u,v)
+                
+                #l(x,y)
+                lxy = (2*meanx*meany + C1)/(meanx**2 + meany**2 + C1)
+                #c(x,y)
+                cxy = (2*msex*msey + C2)/(msex**2 + msey**2 + C2)
+                #s(x,y)
+                sxy = (cc + C3)/(msex*msey + C3)
+                
+                ssim = lxy*cxy*sxy
+                ''' the same funciton to calculate ssim
+                ssim = (2*meanx*meany + C1)*(2*cc + C2)
+                ssim = ssim/(((meanx**2) + (meany**2) + C1)*((msex**2) + (msey**2) + C2))
+                '''
+                ans = ans + ssim
+        ans = ans/((gray.shape[0]/8)*(gray.shape[1]/8))
+        return ans
+    else:
+        raise BaseException("Images are different size")
 @jit
 def Calmse(gray,gray2):
     if(gray.shape[0]==gray2.shape[0]) and (gray.shape[1]==gray2.shape[1]):
